64bd090ca8f598fecdd0dcfe7dfa8271a09c8e24
mparab01 <infoeastindia1@gmail.com>
2016-09-30 22:43:31 -0500

    Python_Tuple
    https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences

---
+++ Tuple.py
@@ -0,0 +1,67 @@
+
+# coding: utf-8
+
+# In[1]:
+
+t = ()
+
+
+# In[2]:
+
+len (t)
+
+
+# In[5]:
+
+t = (1, 'one')
+
+
+# In[4]:
+
+t[0]
+
+
+# In[6]:
+
+len(t)
+
+
+# In[7]:
+
+t[-1]
+
+
+# In[8]:
+
+t[2:]
+
+
+# In[9]:
+
+t[1:]
+
+
+# In[10]:
+
+t.index(1)
+
+
+# In[11]:
+
+t.count(1)
+
+
+# In[12]:
+
+t[2] = "Immutable"
+
+
+# In[13]:
+
+t.append("cantAppend")
+
+
+# In[ ]:
+
+
+
